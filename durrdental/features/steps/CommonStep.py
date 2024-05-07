2ccc5bb43c2130850ef0760ad18e80531fc6ce40
mw <nmw_ng@hotmail.com>
2024-05-07 12:14:11 +0800

    Refactor code

--- durrdental/features/steps/CommonStep.py
+++ durrdental/features/steps/CommonStep.py
@@ -1,5 +1,3 @@
-import time
-
 import WebElements
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -7,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from behave import *
 from selenium import webdriver
 
-TIMEOUT = 10
+TIMEOUT = 30
 
 
 @given('I am on "{url}" - VS Monitor Homepage')
@@ -18,58 +16,37 @@ def i_am_on_vs_monitor_homepage(context, url):
 
 @given('I enter "{username}" for username and "{password}" for password')
 def i_enter_the_credential(context, username, password):
-    element_username = get_username_element(context)
+    element_username = presence_element_located_find_by_id(context, WebElements.GET_USERNAME_INPUT_AT_LOGIN_PAGE)
     element_username.send_keys(username)
 
-    element_password = get_password_element(context)
+    element_password = presence_element_located_find_by_id(context, WebElements.GET_PASSWORD_INPUT_AT_LOGIN_PAGE)
     element_password.send_keys(password)
 
 
 @when('I click on Login button in VS Monitor Homepage')
 def i_click_on_login_button_in_vs_monitor_homepage(context):
-    element = WebDriverWait(context.driver, TIMEOUT).until(
-        EC.visibility_of_element_located((By.XPATH, WebElements.GET_LOGIN_BUTTON_AT_HOMEPAGE)))
+    element = presence_element_located_find_by_xpath(context, WebElements.GET_LOGIN_BUTTON_AT_HOMEPAGE)
     element.click()
 
 
 @when('I click on LOG-IN button on Log-in page')
 def i_click_on_login_button_on_login_page(context):
-    element_login_button = get_login_button_in_login_page(context)
+    element_login_button = presence_element_located_find_by_xpath(context, WebElements.GET_LOGIN_BUTTON_AT_LOGIN_PAGE)
     element_login_button.submit()
 
 
 @then("I validate that I navigate to Login page")
 def i_able_to_validate_that_i_navigate_to_login_page(context):
-    element_login_label = get_login_label_element(context)
-    element_username = get_username_element(context)
-    element_password = get_password_element(context)
-    element_login_button = get_login_button_element(context)
+    element_login_label = presence_element_located_find_by_xpath(context, WebElements.GET_LOGIN_LABEL_AT_LOGIN_PAGE)
+    element_username = presence_element_located_find_by_id(context, WebElements.GET_USERNAME_INPUT_AT_LOGIN_PAGE)
+    element_password = presence_element_located_find_by_id(context, WebElements.GET_PASSWORD_INPUT_AT_LOGIN_PAGE)
+    element_login_button = presence_element_located_find_by_xpath(context, WebElements.GET_LOGIN_BUTTON_AT_LOGIN_PAGE)
 
     if (not element_login_label and not element_username.is_displayed()
             and not element_password.is_displayed() and not element_login_button.is_displayed()):
         get_element_in_login_page_is_displayed(element_login_label, element_password, element_username,
                                                element_login_button)
         raise AssertionError("Element not found")
-
-
-def get_login_label_element(context):
-    return WebDriverWait(context.driver, TIMEOUT).until(
-        EC.presence_of_element_located((By.XPATH, WebElements.GET_LOGIN_LABEL_AT_LOGIN_PAGE)))
-
-
-def get_username_element(context):
-    return WebDriverWait(context.driver, TIMEOUT).until(
-        EC.presence_of_element_located((By.ID, WebElements.GET_USERNAME_INPUT_AT_LOGIN_PAGE)))
-
-
-def get_password_element(context):
-    return WebDriverWait(context.driver, TIMEOUT).until(
-        EC.presence_of_element_located((By.ID, WebElements.GET_PASSWORD_INPUT_AT_LOGIN_PAGE)))
-
-
-def get_login_button_element(context):
-    return WebDriverWait(context.driver, TIMEOUT).until(
-        EC.presence_of_element_located((By.XPATH, WebElements.GET_LOGIN_BUTTON_AT_LOGIN_PAGE)))
 
 
 def get_element_in_login_page_is_displayed(element_login_label, element_password, element_username,
@@ -80,6 +57,17 @@ def get_element_in_login_page_is_displayed(element_login_label, element_password
     print("element_login_button : ", element_login_button.is_displayed())
 
 
-def get_login_button_in_login_page(context):
-    return WebDriverWait(context.driver, TIMEOUT).until(
-        EC.presence_of_element_located((By.XPATH, WebElements.GET_LOGIN_BUTTON_AT_LOGIN_PAGE)))
+def presence_element_located_find_by_xpath(context, web_element):
+    return WebDriverWait(context.driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, web_element)))
+
+
+def presence_element_located_find_by_id(context, web_element):
+    return WebDriverWait(context.driver, TIMEOUT).until(EC.presence_of_element_located((By.ID, web_element)))
+
+
+def visibility_element_located_find_by_xpath(context, web_element):
+    return WebDriverWait(context.driver, TIMEOUT).until(EC.visibility_of_element_located((By.XPATH, web_element)))
+
+
+def visibility_element_located_find_by_id(context, web_element):
+    return WebDriverWait(context.driver, TIMEOUT).until(EC.visibility_of_element_located((By.ID, web_element)))
--- durrdental/features/steps/VistaSoftMonitorLogin.py
+++ durrdental/features/steps/VistaSoftMonitorLogin.py
@@ -1,17 +1,12 @@
 from behave import *
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
+import CommonStep
 import WebElements
-
-TIMEOUT = 30
 
 
 @given('I verify that I successfully login to VS Monitor with "{expected_email}"')
 def i_verify_that_i_successfully_login_to_vs_monitor(context, expected_email):
-    element_email = WebDriverWait(context.driver, TIMEOUT).until(
-        EC.presence_of_element_located((By.XPATH, WebElements.GET_EMAIL_NAVIGATION_MENU)))
+    element_email = CommonStep.presence_element_located_find_by_xpath(context, WebElements.GET_EMAIL_NAVIGATION_MENU)
     print(element_email.text)
 
     if element_email.text != expected_email:
@@ -20,8 +15,7 @@ def i_verify_that_i_successfully_login_to_vs_monitor(context, expected_email):
 
 @then('I verify that the "{error_message}" should be displayed')
 def i_verify_that_the_error_message_should_be_displayed(context, error_message):
-    element_error_message = WebDriverWait(context.driver, TIMEOUT).until(
-        EC.presence_of_element_located((By.ID, WebElements.GET_LOGIN_ERROR_MESSAGE)))
+    element_error_message = CommonStep.presence_element_located_find_by_id(context, WebElements.GET_LOGIN_ERROR_MESSAGE)
 
     if not element_error_message.is_displayed() and element_error_message.text != error_message:
         raise AssertionError("Error message not display")
@@ -29,8 +23,8 @@ def i_verify_that_the_error_message_should_be_displayed(context, error_message):
 
 @then('I verify that I able to navigate to Dashboard at "{expected_url}"')
 def i_validate_that_i_navigate_to_my_user_account(context, expected_url):
-    element_dashboard_label = WebDriverWait(context.driver, TIMEOUT).until(
-        EC.visibility_of_element_located((By.XPATH, WebElements.GET_DASHBOARD_LABEL)))
+    element_dashboard_label = CommonStep.visibility_element_located_find_by_xpath(context,
+                                                                                  WebElements.GET_DASHBOARD_LABEL)
 
     actual_url = context.driver.current_url
 
